c7cf627b46f33d6c6f3a472348bd8bfda0e5b70d
Souvlaki42 <souvlaki420@gmail.com>
2025-06-15 01:32:42 +0300

    feat: change class to className

--- src/features/core/Layout.jsx
+++ src/features/core/Layout.jsx
@@ -7,13 +7,13 @@ export default function Layout() {
       <Outlet />
       {/* This indicator shows which tailwind width breakpoint is currently active in development only. Useful when debugging responsiveness. */}
       {process.env.NODE_ENV === "development" && (
-        <div class="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
-          <div class="block sm:hidden">xs</div>
-          <div class="hidden sm:block md:hidden">sm</div>
-          <div class="hidden md:block lg:hidden">md</div>
-          <div class="hidden lg:block xl:hidden">lg</div>
-          <div class="hidden xl:block 2xl:hidden">xl</div>
-          <div class="hidden 2xl:block">2xl</div>
+        <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+          <div className="block sm:hidden">xs</div>
+          <div className="hidden sm:block md:hidden">sm</div>
+          <div className="hidden md:block lg:hidden">md</div>
+          <div className="hidden lg:block xl:hidden">lg</div>
+          <div className="hidden xl:block 2xl:hidden">xl</div>
+          <div className="hidden 2xl:block">2xl</div>
         </div>
       )}
     </>
