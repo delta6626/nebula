9ac2473233fe7a8cfc67ce05ebefae4034c5a9fa
Delta6626 <hassan911ee@gmail.com>
2025-06-23 21:04:02 +0400

    add content

--- src/features/core/ResourceNotFound.jsx
+++ src/features/core/ResourceNotFound.jsx
@@ -1,5 +1,19 @@
+import NavBar from "../components/NavBar";
+
 function ResourceNotFound() {
-  return <div className=""></div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <div className="flex-grow flex gap-10 items-center justify-center px-6 md:px-20 lg:px-32 xl:px-40 2xl:px-50 text-center font-jakarta">
+        <h1 className="font-extrabold mb-6 text-9xl">404</h1>
+        <p className="text-left max-w-xl text-2xl">
+          Well, this is awkward. The page you’re looking for isn’t here
+          anymore—or maybe it never was. Try checking the URL, or head back home
+          to find what you need.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ResourceNotFound;
