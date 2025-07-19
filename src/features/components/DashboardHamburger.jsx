2a9a94ed389935e64cce5e9e2e8124bcc44386dc
Delta6626 <hassan911ee@gmail.com>
2025-07-20 01:28:57 +0400

    create function to handle hamburger menu close

--- src/features/components/DashboardHamburger.jsx
+++ src/features/components/DashboardHamburger.jsx
@@ -1,11 +1,21 @@
 import { XIcon } from "lucide-react";
+import { useDashboardHamburgerStore } from "../../store/dashboardHamburgerStore";
 
 function DashboardHamburger() {
+  const { dashboardHamburgerOpen, setDashboardHamburgerOpen } =
+    useDashboardHamburgerStore();
+
+  function handleMenuClose() {
+    setDashboardHamburgerOpen(false);
+  }
+
   return (
-    <div className="absolute hidden w-full z-10 h-90 bg-base-100 px-8 py-4">
+    <div
+      className={`absolute ${!dashboardHamburgerOpen ? "hidden" : ""} w-full z-10 h-90 bg-base-100 px-8 py-4`}
+    >
       <div className="">
         <div className="flex items-center text-2xl font-bold gap-2">
-          <button className="btn btn-square">
+          <button className="btn btn-square" onClick={handleMenuClose}>
             <XIcon />
           </button>
           Menu
