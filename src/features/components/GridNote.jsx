a88493a337a99e1b250fe8e28a6c10938f381b9e
Delta6626 <hassan911ee@gmail.com>
2025-07-19 14:42:21 +0400

    responsiveness

--- src/features/components/GridNote.jsx
+++ src/features/components/GridNote.jsx
@@ -131,7 +131,7 @@ function GridNote({ noteObject }) {
 
   return (
     <div
-      className="w-sm bg-base-100 rounded-lg p-4 select-none cursor-pointer"
+      className="w-70 2xl:w-90 bg-base-100 rounded-lg p-4 select-none cursor-pointer"
       onClick={handleNoteClick}
     >
       <div className="flex gap-2 items-center justify-between">
