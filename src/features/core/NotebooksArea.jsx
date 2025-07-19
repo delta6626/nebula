722b3691dfdb8b79233e6d913ff470cf06d587f1
Delta6626 <hassan911ee@gmail.com>
2025-07-20 00:34:27 +0400

    add hamburger menu in notebooks area

--- src/features/core/NotebooksArea.jsx
+++ src/features/core/NotebooksArea.jsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { MenuIcon, Search } from "lucide-react";
 import { useEffect } from "react";
 import { APP_CONSTANTS } from "../../constants/APP_CONSTANTS";
 import { useCurrentNotesViewStore } from "../../store/currentNotesViewStore";
@@ -66,7 +66,12 @@ function NotebooksArea() {
   return (
     <div className="flex-1 bg-base-300 h-[100vh] font-jakarta overflow-y-scroll scroll-smooth scrollbar-thin py-4">
       <div className="flex items-center justify-between px-8 relative">
-        <h1 className="text-2xl font-bold">Notebooks</h1>
+        <div className="flex items-center text-2xl font-bold gap-2">
+          <button className="xl:hidden btn btn-square">
+            <MenuIcon />
+          </button>
+          Notebooks
+        </div>
         <div className="flex absolute left-1/2 -translate-x-1/2">
           <div className="md:w-sm lg:w-lg xl:w-xl 2xl:w-2xl input focus-within:input-primary">
             <Search className="text-secondary" />
