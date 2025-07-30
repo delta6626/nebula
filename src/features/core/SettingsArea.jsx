91d23294ff75d09d4978cc1d875ea0ca396c2fb3
Delta6626 <hassan911ee@gmail.com>
2025-07-30 17:25:26 +0400

    responsive settings page

--- src/features/core/SettingsArea.jsx
+++ src/features/core/SettingsArea.jsx
@@ -798,10 +798,10 @@ function SettingsArea() {
           <p className="text-xl font-semibold">Account</p>
           <p className="mt-4 text-neutral-400">Manage your account</p>
           <div className="divider"></div>
-          <div className="flex justify-between">
+          <div className="flex justify-between xs:flex-row flex-col">
             <p className="font-medium">Send verification mail</p>
             <button
-              className="btn btn-primary"
+              className="btn btn-primary xs:mt-0 mt-2"
               disabled={userVerified ? true : false}
               onClick={handleSendVerificationMail}
             >
@@ -814,10 +814,10 @@ function SettingsArea() {
               )}
             </button>
           </div>
-          <div className="flex justify-between mt-4">
+          <div className="flex justify-between mt-4 xs:flex-row flex-col">
             <p className="font-medium">Sign out</p>
             <button
-              className="btn btn-primary"
+              className="btn btn-primary xs:mt-0 mt-2"
               disabled={signingOut}
               onClick={() => {
                 setMessage({
