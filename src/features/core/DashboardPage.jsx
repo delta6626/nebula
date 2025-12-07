18e279943e11070f12ebb49e6e36741e709f487e
Delta6626 <hassan911ee@gmail.com>
2025-12-08 01:46:39 +0400

    add ui for loading state

--- src/features/core/DashboardPage.jsx
+++ src/features/core/DashboardPage.jsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Tag,
 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/Logo";
@@ -329,149 +329,155 @@ function DashboardPage() {
       <EditNoteModal></EditNoteModal>
       <EditNotebookModal></EditNotebookModal>
       <DashboardHamburger></DashboardHamburger>
-      <div className="flex w-full">
-        <div
-          className={
-            "hidden sideBar h-[100vh] bg-base-300 py-4 xl:flex flex-col ease-in-out duration-200" +
-            (sideBarCollapsed ? " w-20" : " w-60")
-          }
-        >
-          <div className="logo flex items-center justify-between px-4">
-            <Link to={"/home"}>
-              <h1
+      {userLoading || notebooksLoading || notesLoading ? (
+        <div className="flex w-full h-[100vh] items-center justify-center">
+          <span className="loading loading-spinner"></span>
+        </div>
+      ) : (
+        <div className="flex w-full">
+          <div
+            className={
+              "hidden sideBar h-[100vh] bg-base-300 py-4 xl:flex flex-col ease-in-out duration-200" +
+              (sideBarCollapsed ? " w-20" : " w-60")
+            }
+          >
+            <div className="logo flex items-center justify-between px-4">
+              <Link to={"/home"}>
+                <h1
+                  className={
+                    "flex items-center gap-2 text-xl font-jakarta font-bold text-center" +
+                    (sideBarCollapsed ? " hidden" : "")
+                  }
+                >
+                  <Logo />
+                  Nebula
+                </h1>
+              </Link>
+              <button
                 className={
-                  "flex items-center gap-2 text-xl font-jakarta font-bold text-center" +
-                  (sideBarCollapsed ? " hidden" : "")
+                  "btn btn-square bg-transparent" +
+                  (sideBarCollapsed ? " btn-wide rotate-180" : "")
                 }
+                onClick={handleCollapse}
               >
-                <Logo />
-                Nebula
-              </h1>
-            </Link>
-            <button
-              className={
-                "btn btn-square bg-transparent" +
-                (sideBarCollapsed ? " btn-wide rotate-180" : "")
-              }
-              onClick={handleCollapse}
-            >
-              <ArrowLeft></ArrowLeft>
-            </button>
+                <ArrowLeft></ArrowLeft>
+              </button>
+            </div>
+            <div className="divider"></div>
+            <div className="mainButtons flex-1 px-4">
+              <SideBarButton
+                icon={FilePlus}
+                label="New note"
+                onClick={handleNewNoteButtonClick}
+                isDisabled={!userVerified}
+                sideBarCollapsed={sideBarCollapsed}
+              />
+              <SideBarButton
+                icon={BookPlus}
+                label="New notebook"
+                onClick={handleNewNotebookButtonClick}
+                isDisabled={!userVerified}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+
+              <div className="divider" />
+
+              <SideBarButton
+                icon={LayoutPanelTop}
+                label="Dashboard"
+                onClick={handleDashboardButtonClick}
+                isActive={activeTab === APP_CONSTANTS.DASHBOARD_PAGE}
+                sideBarCollapsed={sideBarCollapsed}
+              />
+
+              <SideBarButton
+                icon={File}
+                label="Notes"
+                onClick={handleNotesButtonClick}
+                isActive={activeTab === APP_CONSTANTS.NOTES_PAGE}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+              <SideBarButton
+                icon={Book}
+                label="Notebooks"
+                onClick={handleNotebooksButtonClick}
+                isActive={activeTab === APP_CONSTANTS.NOTEBOOKS_PAGE}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+
+              <div className="divider" />
+
+              <SideBarButton
+                icon={Pin}
+                label="Pinned"
+                onClick={handlePinnedClick}
+                isActive={activeTab === APP_CONSTANTS.PINNED_ITEMS}
+                sideBarCollapsed={sideBarCollapsed}
+              />
+              <SideBarButton
+                icon={Clock}
+                label="Recent"
+                onClick={handleRecentClick}
+                isActive={activeTab === APP_CONSTANTS.RECENT_ITEMS}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+              <SideBarButton
+                icon={Tag}
+                label="Tagged"
+                onClick={handleTaggedClick}
+                isActive={activeTab === APP_CONSTANTS.TAGGED_ITEMS}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+              <SideBarButton
+                icon={UntaggedIcon}
+                label="Untagged"
+                onClick={handleUntaggedClick}
+                isActive={activeTab === APP_CONSTANTS.UNTAGGED_ITEMS}
+                sideBarCollapsed={sideBarCollapsed}
+                addTopMargin
+              />
+            </div>
+
+            <div className="bottomButtons flex flex-col justify-between px-4">
+              <div className="divider" />
+              <SideBarButton
+                icon={Settings}
+                label="Settings"
+                onClick={handleSettingsButtonClick}
+                isActive={activeTab === APP_CONSTANTS.SETTINGS_PAGE}
+                sideBarCollapsed={sideBarCollapsed}
+              />
+            </div>
           </div>
-          <div className="divider"></div>
-          <div className="mainButtons flex-1 px-4">
-            <SideBarButton
-              icon={FilePlus}
-              label="New note"
-              onClick={handleNewNoteButtonClick}
-              isDisabled={!userVerified}
-              sideBarCollapsed={sideBarCollapsed}
-            />
-            <SideBarButton
-              icon={BookPlus}
-              label="New notebook"
-              onClick={handleNewNotebookButtonClick}
-              isDisabled={!userVerified}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-
-            <div className="divider" />
-
-            <SideBarButton
-              icon={LayoutPanelTop}
-              label="Dashboard"
-              onClick={handleDashboardButtonClick}
-              isActive={activeTab === APP_CONSTANTS.DASHBOARD_PAGE}
-              sideBarCollapsed={sideBarCollapsed}
-            />
-
-            <SideBarButton
-              icon={File}
-              label="Notes"
-              onClick={handleNotesButtonClick}
-              isActive={activeTab === APP_CONSTANTS.NOTES_PAGE}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-            <SideBarButton
-              icon={Book}
-              label="Notebooks"
-              onClick={handleNotebooksButtonClick}
-              isActive={activeTab === APP_CONSTANTS.NOTEBOOKS_PAGE}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-
-            <div className="divider" />
-
-            <SideBarButton
-              icon={Pin}
-              label="Pinned"
-              onClick={handlePinnedClick}
-              isActive={activeTab === APP_CONSTANTS.PINNED_ITEMS}
-              sideBarCollapsed={sideBarCollapsed}
-            />
-            <SideBarButton
-              icon={Clock}
-              label="Recent"
-              onClick={handleRecentClick}
-              isActive={activeTab === APP_CONSTANTS.RECENT_ITEMS}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-            <SideBarButton
-              icon={Tag}
-              label="Tagged"
-              onClick={handleTaggedClick}
-              isActive={activeTab === APP_CONSTANTS.TAGGED_ITEMS}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-            <SideBarButton
-              icon={UntaggedIcon}
-              label="Untagged"
-              onClick={handleUntaggedClick}
-              isActive={activeTab === APP_CONSTANTS.UNTAGGED_ITEMS}
-              sideBarCollapsed={sideBarCollapsed}
-              addTopMargin
-            />
-          </div>
-
-          <div className="bottomButtons flex flex-col justify-between px-4">
-            <div className="divider" />
-            <SideBarButton
-              icon={Settings}
-              label="Settings"
-              onClick={handleSettingsButtonClick}
-              isActive={activeTab === APP_CONSTANTS.SETTINGS_PAGE}
-              sideBarCollapsed={sideBarCollapsed}
-            />
+          <div className="divider divider-horizontal m-0"></div>
+          <div className="w-full">
+            {activeTab == APP_CONSTANTS.DASHBOARD_PAGE ? (
+              <DashboardArea></DashboardArea>
+            ) : activeTab == APP_CONSTANTS.NOTES_PAGE ? (
+              <NotesArea></NotesArea>
+            ) : activeTab == APP_CONSTANTS.NOTEBOOKS_PAGE ? (
+              <NotebooksArea></NotebooksArea>
+            ) : activeTab == APP_CONSTANTS.SETTINGS_PAGE ? (
+              <SettingsArea></SettingsArea>
+            ) : activeTab == APP_CONSTANTS.TAGGED_ITEMS ? (
+              <TaggedArea></TaggedArea>
+            ) : activeTab == APP_CONSTANTS.UNTAGGED_ITEMS ? (
+              <UntaggedArea></UntaggedArea>
+            ) : activeTab == APP_CONSTANTS.PINNED_ITEMS ? (
+              <PinnedArea></PinnedArea>
+            ) : activeTab == APP_CONSTANTS.RECENT_ITEMS ? (
+              <RecentArea></RecentArea>
+            ) : (
+              ""
+            )}
           </div>
         </div>
-        <div className="divider divider-horizontal m-0"></div>
-        <div className="w-full">
-          {activeTab == APP_CONSTANTS.DASHBOARD_PAGE ? (
-            <DashboardArea></DashboardArea>
-          ) : activeTab == APP_CONSTANTS.NOTES_PAGE ? (
-            <NotesArea></NotesArea>
-          ) : activeTab == APP_CONSTANTS.NOTEBOOKS_PAGE ? (
-            <NotebooksArea></NotebooksArea>
-          ) : activeTab == APP_CONSTANTS.SETTINGS_PAGE ? (
-            <SettingsArea></SettingsArea>
-          ) : activeTab == APP_CONSTANTS.TAGGED_ITEMS ? (
-            <TaggedArea></TaggedArea>
-          ) : activeTab == APP_CONSTANTS.UNTAGGED_ITEMS ? (
-            <UntaggedArea></UntaggedArea>
-          ) : activeTab == APP_CONSTANTS.PINNED_ITEMS ? (
-            <PinnedArea></PinnedArea>
-          ) : activeTab == APP_CONSTANTS.RECENT_ITEMS ? (
-            <RecentArea></RecentArea>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
